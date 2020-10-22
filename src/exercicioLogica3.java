268de9deead61615080227d2c037ffe300cf8ec2
Eduardo Andrade Santos <eandrasa@everis.com>
2020-10-22 13:31:19 -0300

    first commit

---
+++ src/exercicioLogica3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class exercicioLogica3 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		 
+		double ceucius, fire, conversao, opcao;
+		
+		System.out.println("!! Se não quiser uma temperatura digite 0 !! ");
+		
+		System.out.printf("digite temperatura em ceucius: ");
+		ceucius = (double) ler.nextDouble();
+		System.out.printf("digite temperatura em fire: ");
+		fire = (double) ler.nextDouble();
+		System.out.printf("Converter de C pra F digite opção 1 ou F para C digite 2: ");
+		opcao = (double) ler.nextDouble();
+		
+		if (opcao==1) {
+			conversao=((ceucius*1.8)+32);
+			System.out.println("resultado C>F: "+conversao);
+		} else
+		if (opcao==2) {
+			conversao=((fire-32)/1.8);
+			System.out.println("resultado F>C: "+conversao);
+		}
+		
+	}
+
+}
